7dc2328f49e348d0fe20e21d88c3474f6c2554f3
Ronan Rodrigues <rounantj@hotmail.com>
2022-09-10 13:42:45 -0300

    teste

--- modules/product/product-service.ts
+++ modules/product/product-service.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 import {
   ProductDestroyResponseDTO,
   ProductFetchAllPayloadDTO,
@@ -8,54 +8,54 @@ import {
   ProductSaveResponseDTO,
   ProductUpdatePayloadDTO,
   ProductUpdateResponseDTO,
-} from "./product-interfaces";
-import ProductRepository from "./product-repository";
+} from './product-interfaces'
+import ProductRepository from './product-repository'
 
 export default class productService {
-  private readonly productRepository: ProductRepository;
+  private readonly productRepository: ProductRepository
 
   constructor(public prismaClient: PrismaClient) {
-    this.productRepository = new ProductRepository(prismaClient);
+    this.productRepository = new ProductRepository(prismaClient)
   }
 
   async fetchAll(
     params: ProductFetchAllPayloadDTO
   ): Promise<ProductFetchAllResponseDTO> {
-    const products = await this.productRepository.all(params);
-    return { products };
+    const products = await this.productRepository.all(params)
+    return { products }
   }
 
   async fetchById(
     id: number,
     params: ProductFetchAllPayloadDTO
   ): Promise<ProductFetchOneResponseDTO> {
-    const product = await this.productRepository.get(id, params);
-    return { product };
+    const product = await this.productRepository.get(id, params)
+    return { product }
   }
 
   async fetchBySlug(
     slug: string,
     params: ProductFetchAllPayloadDTO
   ): Promise<ProductFetchOneResponseDTO> {
-    const product = await this.productRepository.getBySlug(slug, params);
-    return { product };
+    const product = await this.productRepository.getBySlug(slug, params)
+    return { product }
   }
 
   async store(payload: ProductSavePayloadDTO): Promise<ProductSaveResponseDTO> {
-    const product = await this.productRepository.save(payload);
-    return { product };
+    const product = await this.productRepository.save(payload)
+    return { product }
   }
 
   async update(
     id: number,
     payload: ProductUpdatePayloadDTO
   ): Promise<ProductUpdateResponseDTO> {
-    const product = await this.productRepository.update(id, payload);
-    return { product };
+    const product = await this.productRepository.update(id, payload)
+    return { product }
   }
 
   async destroy(id: number): Promise<ProductDestroyResponseDTO> {
-    const product = await this.productRepository.delete(id);
-    return { product };
+    const product = await this.productRepository.delete(id)
+    return { product }
   }
 }
